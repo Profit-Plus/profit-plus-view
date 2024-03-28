bb2b69bbf7c6417dbe01095a28054e3bd4c48083
irfanmustahafaa <irfanmustafa2307@gmail.com>
2024-03-28 11:05:12 +0700

    add routes project monitoring

---
+++ src/app/(project-monitoring)/project-monitoring/all-project/page.jsx
@@ -0,0 +1,3 @@
+export default function AllProject() {
+  return <div className="bg-white w-full h-full rounded-xl">AllProject</div>;
+}
--- src/app/(project-monitoring)/project-monitoring/dashboard/page.jsx
+++ src/app/(project-monitoring)/project-monitoring/dashboard/page.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Dashboard() {
   return <div className="bg-white w-full h-full rounded-xl">Dashboard</div>;
 }
